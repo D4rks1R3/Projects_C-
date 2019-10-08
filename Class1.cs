8fc7b7c74978517b21ca4c98fdf0efba165053a2
c4l4ng0_m4t4d0r <40896649+D4rks1R3@users.noreply.github.com>
2019-10-08 15:39:48 -0300

    finalizador de arquivo

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+public class Class1
+{
+	static void Main()
+	{
+        ProcessStartInfo proc = new ProcessStartInfo("ExitAppcom");
+        proc.WindowStyle =  ProcessWindowStyle.Hidden;
+        Process.Start(proc);
+       
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ExitApp
+{
+    class Program
+    {
+     
+    [DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    const int SW_HIDE = 5;
+    static void Main()
+      {
+       const int SW_HIDE = 5;
+       int n = 0;
+       while (true)
+         {
+
+          try
+            {
+             foreach (Process proc in Process.GetProcessesByName("AdobeGCClient"))
+             {
+                        proc.Kill();
+             }
+            }
+
+          catch (Exception exception)
+          {
+                    
+
+          }
+       }
+
+    }
+
+       
+    
+    }
+}
